fe53cdf569ef5fc2a4d2a18e7720feb15e70a723
jaqueline <jaquelinesa.82@gmail.com>
2021-10-08 15:03:04 -0300

    Inserido models aluno e verificação de email.

--- base/views.py
+++ base/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from base.models import Pergunta
+from base.forms import AlunoForm
+from base.models import Pergunta, Aluno
 
 
 def home(request):
+    if request.method == 'POST':
+        # Usúario já existe
+        email = request.POST['email']
+        try:
+            aluno = Aluno.objects.get(email=email)
+        except Aluno.DoesNotExist:
+            # Usúario não existe
+            formulario = AlunoForm(request.POST)
+            if formulario.is_valid():
+                aluno = formulario.save()
+                return redirect('/perguntas/1')
+            else:
+                contexto = {'formulario': formulario}
+                return render(request, 'base/home.html', contexto)
+        else:
+            request.session['aluno_id'] = aluno.id
+            return redirect('/perguntas/1')
     return render(request, 'base/home.html')
 
 
 def perguntas(request, indice):
-    pergunta = \
-        Pergunta.objects.filter(disponivel=True).order_by('id')[indice - 1]
+    pergunta = Pergunta.objects.filter(disponivel=True).order_by('id')[indice - 1]
     contexto = {'indice_da_questao': indice, 'pergunta': pergunta}
     return render(request, 'base/game.html', context=contexto)
 
