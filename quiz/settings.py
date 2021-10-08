759e1eeb0fca722b55a2695b8bc53d2fd7e0a025
jaqueline <jaquelinesa.82@gmail.com>
2021-10-07 21:15:37 -0300

    Criado app do projeto e inserido a pasta static

--- quiz/settings.py
+++ quiz/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'quiz.base.apps',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
