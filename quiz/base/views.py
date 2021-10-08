c3bb317a253f6a8513a1f3d63d3f9d6e3f92f75a
jaqueline <jaquelinesa.82@gmail.com>
2021-10-07 22:17:48 -0300

    Inserido o Frontend do projeto.

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -1,3 +1,14 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'base/home.html')
+
+
+def perguntas(request, indice):
+    contexto = {'indice_da_questao': indice}
+    return render(request, 'base/game.html', context=contexto)
+
+
+def classificacao(request):
+    return render(request, 'base/classificacao.html')
